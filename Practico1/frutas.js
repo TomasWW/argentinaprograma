32803e4bcead2dc985071781ced14c3eb23a62fb
Tomás Woelflin <tomas.woelflin@gmail.com>
2023-06-08 21:02:16 -0300

    PracticoN1

--- Practico1/frutas.js
+++ Practico1/frutas.js
@@ -3,6 +3,6 @@ frutas = [];
 frutas.push("pera", "frutilla", "manzana", "banana", "naranja");
 console.log(frutas);
 let ultima = frutas[frutas.length - 1];
-let primera = frutas.slice(0, 1);
+let primera = frutas[0];
 console.log("La ultima fruta agregada fue: " + JSON.stringify(ultima));
 console.log("La primera fruta agregada fue: " + JSON.stringify(primera));
