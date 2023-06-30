53c99471b40e25f268f58d564d5e563b162b76ff
Tomás Woelflin <tomas.woelflin@gmail.com>
2023-06-30 17:44:18 -0300

    Arreglos varios. Variables en ingles

--- Practico4/Extra2.0.js
+++ Practico4/Extra2.0.js
@@ -1,115 +1,123 @@
 const readlineSync = require("readline-sync");
-const PIEDRA = "Piedra";
-const PAPEL = "Papel";
-const TIJERAS = "Tijeras";
-let USUARIO = readlineSync.question("Hola! Como te llamas? ");
-let GANA_USUARIO = `Gana ${USUARIO}`;
-const GANA_PC = "Gana Computadora";
-const EMPATE = "Empate";
+const ROCK = "Piedra";
+const PAPER = "Papel";
+const SCISSORS = "Tijeras";
+let USER = readlineSync.question(
+  `Hola Bienvenido a Piedra, Papel o Tijera. 
+  Ingrese su nombre: `
+);
+let USER_WINS = `Gano ${USER}`;
+const PC_WINS = "Gano la PC";
+const DRAW = "Empate";
 
-function obtenerJugadaComputadora() {
+function getPcChoice() {
   let num = Math.random() * 3;
   let round = Math.round(num);
-  let compu;
+  let PC;
   if (round == 0) {
-    compu = PIEDRA;
+    PC = ROCK;
   } else if (round == 1) {
-    compu = PAPEL;
-  } else compu = TIJERAS;
+    PC = PAPER;
+  } else PC = SCISSORS;
 
-  return compu;
+  return PC;
 }
 
-function obtenerJugadaUsuario() {
-  let eleccion = parseInt(
-    readlineSync.question("Piedra = 0, Papel = 1 o Tijeras = 2: ")
+function getUserChoice() {
+  let selection = parseInt(
+    readlineSync.question(`
+    > Piedra = 0 
+    > Papel = 1
+    > Tijeras = 2
+    Tu respuetas:  `)
   );
-  const opciones = ["Piedra", "Papel", "Tijeras"];
+  const options = [ROCK, PAPER, SCISSORS];
 
-  while (eleccion < 0 || eleccion > 2) {
-    console.log("Elección inválida. Debes elegir entre 0, 1 o 2.");
-    eleccion = readlineSync.question("Piedra = 0, Papel = 1 o Tijeras = 2: ");
+  while (selection < 0 || selection > 2) {
+    console.log("Eleccion Invalida. Elegi entre-->  ");
+    selection = readlineSync.question("Piedra = 0, Papel = 1 o Tijeras = 2:");
   }
 
-  return opciones[eleccion];
+  return options[selection];
 }
 
-function determinarGanador(compu, usuario) {
-  let resultado;
-  if (compu == usuario) {
-    resultado = EMPATE;
+function determineWinner(PC, user) {
+  let score;
+  if (PC == user) {
+    score = DRAW;
   } else if (
-    (usuario == PIEDRA && compu == TIJERAS) ||
-    (usuario == TIJERAS && compu == PAPEL) ||
-    (usuario == PAPEL && compu == PIEDRA)
+    (user == ROCK && PC == SCISSORS) ||
+    (user == SCISSORS && PC == PAPER) ||
+    (user == PAPER && PC == ROCK)
   ) {
-    resultado = GANA_USUARIO;
+    score = USER_WINS;
   } else {
-    resultado = GANA_PC;
+    score = PC_WINS;
   }
 
   console.log(
-    `La computadora eligio: ${compu}. 
-${USUARIO} eligio: ${usuario}.`
+    `La PC eligio: ${PC}. 
+${USER} eligio: ${user}.`
   );
-  console.log(resultado);
-  return resultado;
+  console.log(score);
+  return score;
 }
 
-function modosJuegos() {
-  let modoJuego = parseInt(
-    readlineSync.question(`Que modo quieres jugar:
-          Una Jugada = 1
-          Dos Jugadas = 2
-          El Mejor de tres = 3
+function gameModes() {
+  let gameMode = parseInt(
+    readlineSync.question(`Hola ${USER}
+    Que modo quieres jugar:
+         > Una Jugada = 1
+         > Dos Jugadas = 2
+         > El Mejor de tres = 3
           `)
   );
-  while (modoJuego < 1 || modoJuego > 3) {
+  while (gameMode < 1 || gameMode > 3) {
     console.log("--------Eleccion Invalida-------");
-    modoJuego = readlineSync.question(`Que modo quieres jugar:
+    gameMode = readlineSync.question(`Que modo quieres jugar:
     Una Jugada = 1
     Dos Jugadas = 2
     El Mejor de tres = 3
     `);
   }
-  let contadorUsuario = 0;
-  let contadorComputadora = 0;
+  let userCounter = 0;
+  let userPC = 0;
 
-  for (let i = 1; i <= modoJuego; i++) {
+  for (let i = 1; i <= gameMode; i++) {
     console.log(`
-    _______Mano Numero= ${i}/${modoJuego}_______
+    _______Ronda ${i}/${gameMode}_______
      `);
-    let compu = obtenerJugadaComputadora();
-    let usuario = obtenerJugadaUsuario();
-    let resultado = determinarGanador(compu, usuario);
-    if (resultado == GANA_PC) {
-      contadorComputadora += 1;
-    } else if (resultado == GANA_USUARIO) {
-      contadorUsuario += 1;
+    let PC = getPcChoice();
+    let user = getUserChoice();
+    let score = determineWinner(PC, user);
+    if (score == PC_WINS) {
+      userPC += 1;
+    } else if (score == USER_WINS) {
+      userCounter += 1;
     }
   }
-  let mensaje = `
+  let message = `
   -----Resultado Final-----
-  >La computadora obtuvo: ${contadorComputadora}.
-  >${USUARIO} obtuvo: ${contadorUsuario}.`;
+  >La PC obtuvo: ${userPC}.
+  >${USER} obtuvo: ${userCounter}.`;
 
-  if (contadorComputadora > contadorUsuario) {
+  if (userPC > userCounter) {
     console.log(
-      `${mensaje}
------Resultado Final-----> ${GANA_PC}
+      `${message}
+-----Marcador Final-----> ${PC_WINS}
 `
     );
-  } else if (contadorComputadora < contadorUsuario) {
+  } else if (userPC < userCounter) {
     console.log(
-      `${mensaje}
------Resultado Final-----> ${GANA_USUARIO}
+      `${message}
+-----Marcador Final-----> ${USER_WINS}
 `
     );
   } else
     console.log(
-      `${mensaje}
------Resulado Final-----> ${EMPATE}.
+      `${message}
+-----Marcador Final-----> ${DRAW}.
 `
     );
 }
-modosJuegos();
+gameModes();
